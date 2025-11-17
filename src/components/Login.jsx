dd7b6ef73f8d7d3a0817ce42e50dba0c9c8f5e18
harSh745-hi <harshpandharpatte@gmail.com>
2025-11-17 16:33:56 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import loginImg from "../assets/login.png";
+import loginImg from "../assests/login.png";
 import Template from './Template';
 
 function Login({ setIsLoggedIn }) {
